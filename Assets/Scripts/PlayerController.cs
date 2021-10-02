0904bf604cf4b4c55fc4b777209759ab7314c7ee
benhowick <44164648+benhowick@users.noreply.github.com>
2021-10-02 16:35:29 +0100

    Added InteractablesScript
    changed playerscript to include interactables but havent had chance to test yet

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
 
     private Vector2 movement;
 
+    private List<ToolScript> _interactables;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,5 +28,41 @@ public class PlayerController : MonoBehaviour
     public void OnMove(InputValue input)
     {
         movement = input.Get<Vector2>() * speed;
+    }
+
+    public void OnFire(InputValue input) 
+    {
+        GameObject ClosestObject = _interactables[0].gameObject;
+        float ClosestDistance = (_interactables[0].gameObject.transform.position - gameObject.transform.position).magnitude;
+
+        for (int i = 1; i < _interactables.Count; i++)
+        {
+            float D = (_interactables[i].gameObject.transform.position - gameObject.transform.position).magnitude;
+            if (D < ClosestDistance)
+            {
+                ClosestObject = _interactables[i].gameObject;
+            }
+        }
+        for (int i = 1; i < _interactables.Count; i++)
+        {
+            _interactables[i].GetComponent<Renderer>().material.color = new Color(0, 0, 1);
+        }
+        ClosestObject.GetComponent<Renderer>().material.color = new Color(0, 1, 0);
+    }
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        ToolScript TS = collision.gameObject.GetComponent<ToolScript>();
+        if (TS)
+            _interactables.Add(TS);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        ToolScript TS = collision.gameObject.GetComponent<ToolScript>();
+        if (TS)
+            _interactables.Remove(TS);
     }
 }
